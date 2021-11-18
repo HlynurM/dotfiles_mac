4f0d8d868ad1b41bcbb5d5c8f745d49c7549cb1b
Hlynur Magnusson <futomatik@gmail.com>
2021-11-18 11:27:45 +0000

    First mac files commit

---
+++ .doom.d/config.el
@@ -0,0 +1,130 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Hlynur Magnusson"
+      user-mail-address "hlynur.magnus@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; (setq doom-font (font-spec :family "Source Code Pro Semibold" :size 12)
+;;       doom-variable-pitch-font (font-spec :family "Source Code Pro Semibold" :size 14))
+(setq doom-font (font-spec :family "Source Code Pro" :size 14)
+     doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 15)
+     doom-unicode-font (font-spec :family "Source Code Pro" :size 14)
+     doom-big-font (font-spec :family "Source Code Pro" :size 16))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; (setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-vibrant)
+;; (setq doom-theme 'doom-spacegrey)
+;; (setq doom-theme 'doom-snazzy)
+;; (setq doom-theme 'doom-rouge)
+;; (setq doom-theme 'doom-outrun-electric)
+;; (setq doom-theme 'doom-oceanic-next)
+;; (setq doom-theme 'doom-monokai-spectrum)
+;; (setq doom-theme 'doom-monokai-pro)
+;; (setq doom-theme 'doom-material)
+;; (setq doom-theme 'doom-henna)
+;; (setq doom-theme 'doom-dracula)
+;; (setq doom-theme 'doom-laserwave)
+(setq doom-theme 'doom-horizon)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+;;
+;; Set up for vterm
+(use-package vterm
+  :ensure t)
+
+;; Treemacs default wisth
+;; (setq treemacs-width 30)
+
+;; add to ~/.doom.d/config.el
+(when-let (dims (doom-store-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-store-put 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
+
+;; Ask what window to choose after split
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
+
+(setq +ivy-buffer-preview t)
+
+;; Adding Arrows to influence window rotation as an option
+;; Window rotation is nice, and can be found under SPC w r and SPC w R.
+;; Layout rotation is also nice though. Let’s stash this under SPC w SPC,
+;; inspired by Tmux’s use of C-b SPC to rotate windows.
+
+;; We could also do with adding the missing arrow-key variants of
+;; the window navigation/swapping commands.
+(map! :map evil-window-map
+      "SPC" #'rotate-layout
+      ;; Navigation
+      "<left>"     #'evil-window-left
+      "<down>"     #'evil-window-down
+      "<up>"       #'evil-window-up
+      "<right>"    #'evil-window-right
+      ;; Swapping windows
+      "C-<left>"       #'+evil/window-move-left
+      "C-<down>"       #'+evil/window-move-down
+      "C-<up>"         #'+evil/window-move-up
+      "C-<right>"      #'+evil/window-move-right)
