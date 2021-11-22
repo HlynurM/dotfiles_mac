36a254eb9cc2d251530530f434d883ee62846368
Hlynur Magnusson <futomatik@gmail.com>
2021-11-22 10:24:30 +0000

    adding simple aliases for zsh

---
+++ aliases.zsh
@@ -0,0 +1,11 @@
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias q=exit
+alias clr=clear
+alias rm='rm -i'
+alias cp='cp -i'
+alias mb='mv -i'
+alias ports='netstat -tulanp'
+
