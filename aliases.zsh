7ec129111a85d46cbf506c432c1f389c4866e979
Hlynur Magnusson <hlynur@Hlynur-imac.local>
2021-12-09 11:20:02 +0000

    fixed aliases and added iterm.json file

--- aliases.zsh
+++ aliases.zsh
@@ -2,10 +2,21 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
+alias ll="ls -asl"
+
 alias q=exit
 alias clr=clear
 alias rm='rm -i'
 alias cp='cp -i'
 alias mb='mv -i'
 alias ports='netstat -tulanp'
+alias sc="source $HOME/.zshrc"
+
+alias dev="cd ~/DEV/"
+
+
+aliasname() {
+	command $firstParam $secondParam
+}
+
 
